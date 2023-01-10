c2b0fe0b0304b0224998cc8ac5df69758d073cd2
kaderkaraogr <75438365+kaderkaraogr@users.noreply.github.com>
2023-01-10 18:39:08 +0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'egitim-portali-6c8b4',
+    appId: '1:239457607227:web:643889bc6a350a7c46061e',
+    storageBucket: 'egitim-portali-6c8b4.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD0DFDA7WjG7Cw8IAXYH6Sfx0cSZEamr1s',
+    authDomain: 'egitim-portali-6c8b4.firebaseapp.com',
+    messagingSenderId: '239457607227',
+    measurementId: 'G-1KY6JWYZ70',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'egitim-portali-6c8b4',
+    appId: '1:239457607227:web:643889bc6a350a7c46061e',
+    storageBucket: 'egitim-portali-6c8b4.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD0DFDA7WjG7Cw8IAXYH6Sfx0cSZEamr1s',
+    authDomain: 'egitim-portali-6c8b4.firebaseapp.com',
+    messagingSenderId: '239457607227',
+    measurementId: 'G-1KY6JWYZ70',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
